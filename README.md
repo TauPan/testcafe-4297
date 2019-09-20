bda976559cd0099b3217640b7f4740ca7777b1d1
Friedrich Delgado <delgado@dfn-cert.de>
2019-09-20 13:48:12 +0200

    check if it matters that the call is asynchronous

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Github Project: https://github.com/TauPan/testcafe-4297/
 
 Published at: https://taupan.github.io/testcafe-4297/
 
-<script src="index.js">
+<script src="index.js"></script>
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const foo = document.getElementById('#doesnotexist');
-console.log(`Foo: ${foo.style}`);
+async function bar() {
+  const foo = document.getElementById('#doesnotexist');
+  console.log(`Foo: ${foo.style}`);
+}
+
+async function boo() {
+  await bar();
+}
+
+boo();
